77f96fc320aac4e635b2b05ac8e13df0ab21f2dd
Tsai Huang <tsaihuangsd@gmail.com>
2018-10-01 13:29:51 -0700

    Update github

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+const cors = require('cors');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(cors());
+
+server.get('/', (request, response) => {response.send('<h1>Hello FSW13!</h1>');
+});
+
+server.get('/api/users', (request, response) => {
+    db.find()
+    .then(users => {
+        console.log('\n** users **', users);
+        response.json(users);
+    })
+    .catch(err => response.send(err));
+});
+
+const port = 8000;
+server.listen(port, () => 
+    console.log(`\n=== API running on port ${port} === \n`));
